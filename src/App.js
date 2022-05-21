7153b1fd71276f4bf71842c47a81f2c2983e5e93
Farhan <farhansafin@gmail.com>
2022-05-21 21:05:49 +0600

    Nav bar init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { Route, Routes } from 'react-router';
 import './App.css';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login/Login';
+import Navbar from './Pages/Shared/Navbar/Navbar';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home></Home>}></Route>
-      <Route path='/home' element={<Home></Home>}></Route>
-    </Routes>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
+    </div>
   );
 }
 
