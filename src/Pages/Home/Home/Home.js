b98af739176cc787a79d08b9f0ab22ff4ead81a0
Farhan <farhansafin@gmail.com>
2022-05-22 13:36:40 +0600

    Business summary added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner'
+import Business from '../Business/Business';
 import Parts from '../Parts/Parts'
 
 const Home = () => {
     return (
         <div>
         <Banner></Banner>
+        <Business></Business>
         <Parts></Parts>
         </div>
     );
