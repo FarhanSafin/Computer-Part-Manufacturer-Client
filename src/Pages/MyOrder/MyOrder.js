d3a63d0c1619d82afda9185c9403cdf96adf21a2
Farhan <farhansafin@gmail.com>
2022-05-24 21:15:48 +0600

    Individual my order partially done

--- src/Pages/MyOrder/MyOrder.js
+++ src/Pages/MyOrder/MyOrder.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading/Loading';
 
 const MyOrder = () => {
+    const [user, loading] = useAuthState(auth);
+    const [parts, setParts] = useState([]);
+    useEffect(() => {
+        if(user){
+            fetch(`http://localhost:5000/myItems?email=${user.email}`)
+            .then(res => res.json())
+            .then(data => setParts(data))
+        }
+    })
+
     return (
-        <div>
-            <h2>MY ORDERS</h2>
-        </div>
-    );
+            <div>
+                <div>hi {parts.length}</div>
+                <div>{user.email}</div>
+            </div>
+        )
 };
 
 export default MyOrder;
