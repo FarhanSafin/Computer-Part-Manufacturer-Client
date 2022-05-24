09044896fe5f3a037754422c2d9f8e83897fd26b
Farhan <farhansafin@gmail.com>
2022-05-24 20:24:39 +0600

    Order added to database added

--- src/Pages/PartDetail/PartDetail.js
+++ src/Pages/PartDetail/PartDetail.js
@@ -10,7 +10,6 @@ const PartDetail = () => {
     const [user] = useAuthState(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
     const {partId} = useParams();
-    const [agree, setAgree] = useState(true)
     const [part, setPart] = useState({});
     useEffect(() => {
         const url = `http://localhost:5000/part/${partId}`;
@@ -19,8 +18,33 @@ const PartDetail = () => {
         .then(data => setPart(data));
     }, []);
 
-    const handleOrder = () => {
-        toast.success('Order Successfull', {
+    const handleOrder = data => {
+      const minAmount = parseInt(part.minimum);
+      const available = parseInt(part.available);
+      let orderedAmount = parseInt(document.getElementById("amount").value);
+      if(orderedAmount >= minAmount && orderedAmount <= available){
+        const url = `http://localhost:5000/addorder`;
+        fetch(url, { 
+            method: 'POST',
+            headers:{
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(result => {
+          toast.success('Order Successfull', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            });
+        })
+      }else{
+        toast.warn('Please recheck the ordered amount', {
           position: "top-right",
           autoClose: 5000,
           hideProgressBar: false,
@@ -28,101 +52,76 @@ const PartDetail = () => {
           pauseOnHover: true,
           draggable: true,
           progress: undefined,
-          })
-    }
-
-    const handleChange = event => {
-      const minAmount = parseInt(part.minimum);
-      const newMinimum = parseInt(event.target.value);
-      const available = parseInt(part.available);
-      console.log(minAmount, newMinimum);
-      if(newMinimum < minAmount || newMinimum > available) {
-        setAgree(false)
-      }else{
-        setAgree(true)
+          });
       }
     }
 
+
     return (
-      <div>
-      <div class="hero min-h-fit bg-base-200">
-      <div class="hero-content flex-col lg:flex-row-reverse">
-        <img src="https://api.lorem.space/image/movie?w=260&h=400" class="max-w-sm rounded-lg shadow-2xl" />
         <div>
-          <h1 class="text-5xl font-bold text-center">Name: <br></br>{part.name}</h1>
-          <div className='text-2xl text-center'>
-            <p>Description: {part.description}</p>
-            <p>Available: {part.available}</p>
-            <p>Minimum: {part.minimum}</p>
-            <p>Price: {part.price}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className='text-center text-2xl'>
-      <h2>CheckOut Form</h2>
+        <div className="hero min-h-screen" style={{backgroundImage: "url(" + "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" + ")"}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Name: {part.name}</h1>
+      <p className="mb-5">Description: {part.description}.</p>
+      <p className="mb-5">Available: {part.available}.</p>
+      <p id='min' className="mb-5">Minimum Order: {part.minimum}.</p>
+      <p className="mb-5">Price Per Unit: {part.price}.</p>
+      <div className='mx-16'>
+      <form className='text-center' onSubmit={handleSubmit(handleOrder)}>
 
-
-      <form className='text-center md:mx-96 md:px-96' onSubmit={handleSubmit(handleOrder)}>
-
-      <div className="form-control w-full max-w-xs mx-6">
+      <div className="form-control w-full max-w-xs">
 <label className="label">
-<span className="label-text">Selected Item</span>
+<span className="label-text">User</span>
 </label>
-<input type="text" value={part.name} className="input input-bordered w-full max-w-xs" />
+<input type="text" placeholder="Your Name" value={user.displayName} className="input input-bordered w-full max-w-xs" {...register("name")}/>
+</div>
 
-
+<div className="form-control w-full max-w-xs">
+<label className="label">
+<span className="label-text">User's Email</span>
+</label>
+<input type="text" placeholder="Your Name" className="input input-bordered w-full max-w-xs" value={user.email} {...register("email")}/>
 </div>
 
 
-<div className="form-control w-full max-w-xs mx-6">
+<div className="form-control w-full max-w-xs">
 <label className="label">
 <span className="label-text">Address</span>
 </label>
-<input type="text" placeholder="Your Name" className="input input-bordered w-full max-w-xs" {...register("address", {
-  required: {
-      value: true,
-      message: 'Address is required'
-  }
-})} />
-<label className="label">
-{errors.name?.type === 'required' && <span className="label-text-alt text-red-700">{errors.name.message}</span>}
-
-</label>
+<input type="text" placeholder="Your Address" className="input input-bordered w-full max-w-xs" required {...register("address")}/>
 </div>
 
 
-<div className="form-control w-full max-w-xs mx-6">
+<div className="form-control w-full max-w-xs">
 <label className="label">
 <span className="label-text">Mobile Number</span>
 </label>
-<input type="number" placeholder="Your number" className="input input-bordered w-full max-w-xs" {...register("mobile", {
-  required: {
-      value: true,
-      message: 'Number is required'
-  }
-})} />
+<input type="number" placeholder="Your number" className="input input-bordered w-full max-w-xs" required {...register("mobile")}/>
 
 </div>
 
-<div className="form-control w-full max-w-xs mx-6">
+<div className="form-control w-full max-w-xs">
 <label className="label">
 <span className="label-text">Ordered Amount</span>
 </label>
-<input id='amount' type="number" onChange={handleChange} defaultValue={part.minimum} className="input input-bordered w-full max-w-xs" />
+<input id='amount' type="number" placeholder={part.minimum} className="input input-bordered w-full max-w-xs" required {...register("ordered")}/>
 
 
 </div>
 
 
-  <input className='btn w-full max-w-xs mt-5' disabled={!agree} type="submit" value="Order" />
+  <input className='btn w-full max-w-xs mt-5' type="submit" value="Order" />
 </form>
+</div>
+    </div>
+  </div>
+  
+</div>
 <ToastContainer />
-    </div>
-    </div>
+</div>
     );
 };
 
 export default PartDetail;
-
-
