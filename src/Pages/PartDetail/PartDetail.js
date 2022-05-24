7df3afb8bbf35e536787908a77d733c3b4b0b47a
Farhan <farhansafin@gmail.com>
2022-05-24 19:14:02 +0600

    Button disable upon ordered item

--- src/Pages/PartDetail/PartDetail.js
+++ src/Pages/PartDetail/PartDetail.js
@@ -10,6 +10,7 @@ const PartDetail = () => {
     const [user] = useAuthState(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
     const {partId} = useParams();
+    const [agree, setAgree] = useState(true)
     const [part, setPart] = useState({});
     useEffect(() => {
         const url = `http://localhost:5000/part/${partId}`;
@@ -19,10 +20,6 @@ const PartDetail = () => {
     }, []);
 
     const handleOrder = () => {
-      const minAmount = parseInt(part.minimum);
-      const available = parseInt(part.available);
-      let orderedAmount = parseInt(document.getElementById("amount").value);
-      if(orderedAmount >= minAmount && orderedAmount <= available){
         toast.success('Order Successfull', {
           position: "top-right",
           autoClose: 5000,
@@ -31,40 +28,54 @@ const PartDetail = () => {
           pauseOnHover: true,
           draggable: true,
           progress: undefined,
-          });
+          })
+    }
+
+    const handleChange = event => {
+      const minAmount = parseInt(part.minimum);
+      const newMinimum = parseInt(event.target.value);
+      const available = parseInt(part.available);
+      console.log(minAmount, newMinimum);
+      if(newMinimum < minAmount || newMinimum > available) {
+        setAgree(false)
       }else{
-        toast.warn('Please recheck the ordered amount', {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          });
+        setAgree(true)
       }
     }
 
     return (
+      <div>
+      <div class="hero min-h-fit bg-base-200">
+      <div class="hero-content flex-col lg:flex-row-reverse">
+        <img src="https://api.lorem.space/image/movie?w=260&h=400" class="max-w-sm rounded-lg shadow-2xl" />
         <div>
-        <div className='text-center text-xl font-semibold'>
-            <h2>Logged In User: {user.displayName}</h2>
-            <h2>User Email: {user.email}</h2>
+          <h1 class="text-5xl font-bold text-center">Name: <br></br>{part.name}</h1>
+          <div className='text-2xl text-center'>
+            <p>Description: {part.description}</p>
+            <p>Available: {part.available}</p>
+            <p>Minimum: {part.minimum}</p>
+            <p>Price: {part.price}</p>
+          </div>
         </div>
-        <div className="hero min-h-screen" style={{backgroundImage: "url(" + "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" + ")"}}>
-  <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Name: {part.name}</h1>
-      <p className="mb-5">Description: {part.description}.</p>
-      <p className="mb-5">Available: {part.available}.</p>
-      <p id='min' className="mb-5">Minimum Order: {part.minimum}.</p>
-      <p className="mb-5">Price Per Unit: {part.price}.</p>
-      <div className='mx-16'>
-      <form className='text-center' onSubmit={handleSubmit(handleOrder)}>
+      </div>
+    </div>
+    <div className='text-center text-2xl'>
+      <h2>CheckOut Form</h2>
 
 
-<div className="form-control w-full max-w-xs">
+      <form className='text-center md:mx-96 md:px-96' onSubmit={handleSubmit(handleOrder)}>
+
+      <div className="form-control w-full max-w-xs mx-6">
+<label className="label">
+<span className="label-text">Selected Item</span>
+</label>
+<input type="text" value={part.name} className="input input-bordered w-full max-w-xs" />
+
+
+</div>
+
+
+<div className="form-control w-full max-w-xs mx-6">
 <label className="label">
 <span className="label-text">Address</span>
 </label>
@@ -81,7 +92,7 @@ const PartDetail = () => {
 </div>
 
 
-<div className="form-control w-full max-w-xs">
+<div className="form-control w-full max-w-xs mx-6">
 <label className="label">
 <span className="label-text">Mobile Number</span>
 </label>
@@ -94,48 +105,24 @@ const PartDetail = () => {
 
 </div>
 
-<div className="form-control w-full max-w-xs">
+<div className="form-control w-full max-w-xs mx-6">
 <label className="label">
 <span className="label-text">Ordered Amount</span>
 </label>
-<input id='amount' type="number" defaultValue={part.minimum} className="input input-bordered w-full max-w-xs" {...register("order", {
-  required: {
-      value: true,
-      message: 'Order Number is required'
-  }
-})} />
+<input id='amount' type="number" onChange={handleChange} defaultValue={part.minimum} className="input input-bordered w-full max-w-xs" />
 
 
 </div>
 
 
-  <input className='btn w-full max-w-xs mt-5' type="submit" value="Order" />
+  <input className='btn w-full max-w-xs mt-5' disabled={!agree} type="submit" value="Order" />
 </form>
-</div>
-    </div>
-  </div>
-  
-</div>
 <ToastContainer />
-</div>
+    </div>
+    </div>
     );
 };
 
 export default PartDetail;
 
 
-
-
-
-/* ,
-pattern: {
-  value: /[part.minimum-part.available]/,
-  message: 'Cannt Place Order'
-}
-
-<label className="label">
-{errors.order?.type === 'required' && <span className="label-text-alt text-red-700">{errors.order.message}</span>}
-
-{errors.order?.type === 'pattern' && <span className="label-text-alt text-red-700">{errors.order.message}</span>}
-
-</label> */
